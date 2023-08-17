430f044fd6cab107c500c4de1aedb5a1b1bdafed
Teddy Pascual <teddylayson2000@gmail.com>
2023-08-17 19:06:45 +0800

    first commit

---
+++ api/routes/orders.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res, next) => {
+  res.status(200).json({
+    message: "Handling get req to orders",
+  });
+});
+router.post("/", (req, res, next) => {
+  res.status(201).json({
+    message: "Handling post req to orders",
+  });
+});
+
+router.get("/:orderId", (req, res, next) => {
+  const id = req.params.orderId;
+  if (id === "Ted") {
+    res.status(200).json({
+      message: "Omkei",
+    });
+  } else {
+    res.status(200).json({
+      message: "Not Omkei",
+    });
+  }
+});
+
+router.patch("/:orderId", (req, res, next) => {
+  res.status(200).json({
+    message: "Update Goods",
+  });
+});
+
+router.delete("/:orderId", (req, res, next) => {
+  res.status(200).json({
+    message: "Delete Goods",
+  });
+});
+module.exports = router;
---
+++ api/routes/products.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res, next) => {
+  res.status(200).json({
+    message: "Handling get req to products",
+  });
+});
+router.post("/", (req, res, next) => {
+  res.status(200).json({
+    message: "Handling post req to products",
+  });
+});
+
+router.get("/:productId", (req, res, next) => {
+  const id = req.params.productId;
+  if (id === "Ted") {
+    res.status(200).json({
+      message: "Omkei",
+    });
+  } else {
+    res.status(200).json({
+      message: "Not Omkei",
+    });
+  }
+});
+
+router.patch("/:productId", (req, res, next) => {
+  res.status(200).json({
+    message: "Update Goods",
+  });
+});
+
+router.delete("/:productId", (req, res, next) => {
+  res.status(200).json({
+    message: "Delete Goods",
+  });
+});
+module.exports = router;
