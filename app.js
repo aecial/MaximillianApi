9db93744000555254eaa683dad9f4f32a4c85f21
Teddy Pascual <teddylayson2000@gmail.com>
2023-08-18 08:49:31 +0800

    added cors and body parser for post

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
-
+const bodyParser = require("body-parser");
 const productRoutes = require("./api/routes/products");
 const orderRoutes = require("./api/routes/orders");
 
 app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method == "OPTIONS") {
+    req.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
+//extract json data
+app.use(bodyParser.json());
 // for products and order
 app.use("/products", productRoutes);
 app.use("/orders", orderRoutes);
